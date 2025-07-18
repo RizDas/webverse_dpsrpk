48e8124c43001944dc3ad30f5747b002ed4afea3
RizDas <dkkrishabh@gmail.com>
2025-07-18 10:04:41 +0530

    Added pages and connected to app.jsx, added poppins font

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Tours from "./pages/Tours";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -9,7 +14,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={"Hello"} />
+          <Route path="/" element={<Home />} />
+          <Route path="/tours" element={<Tours />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<div>404 Not Found</div>} />
         </Routes>
         <Footer />
       </Router>
